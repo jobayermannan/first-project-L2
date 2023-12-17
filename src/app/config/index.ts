770a05010fde95ee94ac0fd8b421133ca4296f8d
Xenon Xubayer <jobayermannan777@gmail.com>
2023-12-17 08:48:38 +0600

    basic project set up done

---
+++ src/app/config/index.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+
+import path from 'path';
+
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+
+export default {
+  port: process.env.PORT,
+  data_base_url: process.env.DATA_BASE_URL,
+};
